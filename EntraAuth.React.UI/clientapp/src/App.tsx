36bfc6dc596f5824702416000596f44bdc71b5da
Nayan <nynmistry08@gmail.com>
2025-06-20 18:23:09 +0530

    Updated app.tsx file

--- EntraAuth.React.UI/clientapp/src/App.tsx
+++ EntraAuth.React.UI/clientapp/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save.
+          Edit <code>src/App.tsx</code> and save and update.
         </p>
       </div>
       <p className="read-the-docs">
